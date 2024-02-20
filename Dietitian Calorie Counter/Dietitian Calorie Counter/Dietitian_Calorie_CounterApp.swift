43c8c239c09bad1495fd1ca4c6a197f47b2d13eb
Duhan Boblanlı <77344408+duhanboblanli@users.noreply.github.com>
2024-02-20 17:56:47 +0300

    init

---
+++ Dietitian Calorie Counter/Dietitian Calorie Counter/Dietitian_Calorie_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dietitian_Calorie_CounterApp.swift
+//  Dietitian Calorie Counter
+//
+//  Created by Duhan Boblanlı on 20.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dietitian_Calorie_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
